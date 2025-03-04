2ef8577b247649f5b137b09ece4d39c4c2141243
HwangSub <chohs815@naver.com>
2025-03-04 16:45:10 +0900

    2025-03-04

---
+++ myThread/MyThreadTest3.java
@@ -0,0 +1,22 @@
+package myThread;
+
+public class MyThreadTest3 extends Thread {
+	public void run() {
+		for(int i=1; i<=5; i++) {
+			System.out.println("호떡 " + i + "개");
+			try {
+				Thread.sleep(300);
+			} catch (Exception e) { }
+		}
+	}
+	public static void main(String[] args) {
+		MyThreadTest3 my3 = new MyThreadTest3();
+		my3.start();
+		for(int i=1; i<=5; i++) {
+			System.out.println("개떡 " + i + "개");
+			try {
+				Thread.sleep(300);
+			} catch (Exception e) { }
+		}
+	}
+}
